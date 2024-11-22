1bc2cbb6e03ee547d52fe6b29a2638fb420f6fa1
suhelkhanca <suhel.hata@gmail.com>
2024-11-22 22:45:51 +0530

    iterating dictionary using .items() and for loop

---
+++ Lab-Exam/dictionary.py
@@ -0,0 +1,11 @@
+# Program demonstrating dictionary
+my_dict = {
+    "a": 12,
+    "b": 54,
+    "c": 12,
+    "d": 58,
+    "e": 20,
+}
+
+for key, value in my_dict.items():
+    print(key, value)
